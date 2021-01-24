6916480dda397d7268f7a6aaae3f07ea17aff149
Flyworkz <pedron.dorian@gmail.com>
2021-01-24 10:51:42 +0100

    Model Pop OK

---
+++ Models/Pop.cs
@@ -0,0 +1,10 @@
+namespace OhMyPops.Models
+{
+    public class Pop
+    {
+        public int Id { get; set; }
+        public int FigurineNumber { get; set; }
+        public string Collection { get; set; }
+        public string Label { get; set; }
+    }
+}
